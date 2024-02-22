0570286e77e7af8aae0cdabf9a6de70f24820668
Work <Work@DESKTOP-8A6U3T6.(none)>
2024-02-22 11:08:01 +0500

    updates

--- cron_job.py
+++ cron_job.py
@@ -795,7 +795,7 @@ def is_weekday():
 if __name__ == "__main__":
     print("Program started...")
     # is_weekday()
-    schedule.every().day.at("06:03").do(is_weekday)
+    schedule.every().day.at("06:08").do(is_weekday)
 
     # Main loop
     while True:
